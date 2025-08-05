05635ac703fc445d068cee7c9a700c311cf27f04
Sajib <107872992+asmsajib789@users.noreply.github.com>
2025-08-06 01:33:06 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-function showPopup(type) {
-  const popup = document.getElementById('popup');
-  const title = document.getElementById('popup-title');
-  if (type === 'deposit') {
-    title.innerText = '💳 ডিপোজিট সার্ভিস';
-  } else {
-    title.innerText = '🏧 উইথড্র সার্ভিস';
-  }
-  popup.style.display = 'block';
-}
-
-function hidePopup() {
-  document.getElementById('popup').style.display = 'none';
-}
 
