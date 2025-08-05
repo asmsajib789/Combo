026dbb01e2476d90ca5fbbf4a85fd9d80496ccad
Sajib <107872992+asmsajib789@users.noreply.github.com>
2025-08-06 01:30:43 +0600

    Update script.js

--- script.js
+++ script.js
@@ -12,11 +12,4 @@ function showPopup(type) {
 function hidePopup() {
   document.getElementById('popup').style.display = 'none';
 }
-<!-- JS to show popup on page load -->
-<script>
-  window.onload = function() {
-    setTimeout(function() {
-      document.getElementById('popup').style.display = 'block';
-    }, 1500); // 1.5 সেকেন্ড পর পপআপ দেখাবে
-  }
-</script>
+
